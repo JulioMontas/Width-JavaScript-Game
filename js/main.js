30196ccb69672489d8ba5f26020ff7de8c5debe0
JulioMontas <julio86@gmail.com>
2016-03-04 14:37:48 -0500

    new update

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ Round -Zero
 Round +Zero
 01.[/] Make the Global variables
 02.[x] Build the start game function to press the start html and start the game running.
-03.[] start(); then turn the html tag "HEADER" to display = "flex"
+03.[x] start(); then turn the html tag "HEADER" to display = "flex"
 04.[] start(); Turn off the html tag "#gameTable" to none, is only for display purpose
 05.[]
 06.[]
@@ -47,8 +47,32 @@ var header = document.querySelector('header');
 var menu = document.querySelector('nav');
 var displayGameTable = document.querySelector('#gameTable');
 
+
+
+
+
+
+var randomOne = Math.ceil(Math.random() * 100);
+var playerOneHits = randomOne + "%";
+
 var playerOneWidth = document.querySelectorAll('.playerOne');
+for (var i = 0; i < playerOneWidth.length; i++) {
+  playerOneWidth[i].style.width = playerOneHits;
+}
+
+
+
+var randomTwo = Math.ceil(Math.random() * 100);
+var playerTwoHits = randomTwo + "%";
+
 var playerTwoWidth = document.querySelectorAll('.playerTwo');
+for (var i = 0; i < playerTwoWidth.length; i++) {
+  playerTwoWidth[i].style.width = playerTwoHits;
+}
+
+
+
+
 
 
 // Lets lets play
@@ -68,5 +92,27 @@ function gameStartDisplay() {
   console.log("Is Header in the MFH!!!");
   header.style.display = 'flex';
   menu.style.display = 'none';
-  displayGameTable.style.display = 'none';
+  randomOne = 0
+  randomTwo = 0
 }
+
+
+
+
+
+
+
+
+
+
+window.addEventListener("keydown", function(e){
+/*
+keyCode: 8
+keyIdentifier: "U+0008"
+*/
+    if(e.keyCode === 13 && document.activeElement !== 'text') {
+        e.preventDefault();
+        alert('Prevent page from going back');
+        randomTwo += 1;
+    }
+});
