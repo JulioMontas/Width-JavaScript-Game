65c1afab86756939a145c03a351927dfbc237b6a
JulioMontas <julio86@gmail.com>
2016-03-04 16:37:41 -0500

    almost done, I think

--- js/main.js
+++ js/main.js
@@ -7,51 +7,14 @@ console.log("O~~      O~~        O~~  O~~~~~~ O~~    O~~    O~~  O~~  O~~   O~ O
 console.log("O~~        O~~     O~~  O~~       O~~   O~~   O~~   O~~  O~~    O~ ~~   O~~    O~ ")
 console.log("O~~~~~~~~    O~~~~     O~~         O~~  O~~~~~      O~~  O~~      O~~    O~~~~~   ")
 
-/*
-
-Round -Zero
-01.[] Render a game in the browser
-02.[] Switch turns between two players
-03.[] Design logic for winning & visually display which player won
-04.[x] Include separate HTML / CSS / JavaScript files
-05.[] Stick with KISS (Keep It Simple Stupid) and DRY (Don't Repeat Yourself) principles
-06.[] Use Javascript for DOM manipulation
-07.[] Deploy your game online, where the rest of the world can access it
-08.[] Use semantic markup for HTML and CSS (adhere to best practices)
-09.[]
-10.[]
-11.[]
-12.[]
-
-
-Round +Zero
-01.[/] Make the Global variables
-02.[x] Build the start game function to press the start html and start the game running.
-03.[x] start(); then turn the html tag "HEADER" to display = "flex"
-04.[] start(); Turn off the html tag "#gameTable" to none, is only for display purpose
-05.[]
-06.[]
-07.[]
-08.[]
-09.[]
-
-*/
-
 // Calling all Global variables
 var started = false;
-var scoreOne = [];
-var scoreTwo = [];
-var gameOptions = [];
-
 var header = document.querySelector('header');
 var menu = document.querySelector('nav');
+
 var displayGameTable = document.querySelector('#gameTable');
 
-
-
-
-
-
+// Calling all Global variables of player one
 var randomOne = Math.ceil(Math.random() * 100);
 var playerOneHits = randomOne + "%";
 
@@ -60,8 +23,7 @@ for (var i = 0; i < playerOneWidth.length; i++) {
   playerOneWidth[i].style.width = playerOneHits;
 }
 
-
-
+// Calling all Global variables of player Two
 var randomTwo = Math.ceil(Math.random() * 100);
 var playerTwoHits = randomTwo + "%";
 
@@ -70,49 +32,85 @@ for (var i = 0; i < playerTwoWidth.length; i++) {
   playerTwoWidth[i].style.width = playerTwoHits;
 }
 
+//////////////////////
+//                  //
+//  Lets lets play  //
+//                  //
+//////////////////////
 
-
-
-
-
-// Lets lets play
 var playButton = document.querySelector('#play');
 playButton.addEventListener('click', function() {
   start();
 });
 
-// Load up everything that you need for the games. ( Your Treasure Box )
+////////////////////////////////////////////////////////////////////////////
+//                                                                        //
+// Load up everything that you need for the games. ( Your Treasure Box )  //
+//                                                                        //
+////////////////////////////////////////////////////////////////////////////
+
 function start() {
   console.log("Let's Play <--- WIDTH --->");
   started = true;
-  gameStartDisplay();
+  gameReset();
+  gameControl();
 }
 
-function gameStartDisplay() {
+//////////////////////////////////////
+//                                  //
+//  Reset the game to a fresh game  //
+//                                  //
+//////////////////////////////////////
+
+function gameReset() {
+
   console.log("Is Header in the MFH!!!");
   header.style.display = 'flex';
   menu.style.display = 'none';
-  randomOne = 0
-  randomTwo = 0
+
+  randomOne = 0;
+  for (var i = 0; i < playerOneWidth.length; i++) {
+    playerOneWidth[i].style.width = randomOne + '%';
+  }
+  randomTwo = 0;
+  for (var i = 0; i < playerTwoWidth.length; i++) {
+    playerTwoWidth[i].style.width = randomTwo + '%';
+  }
+
 }
 
+/////////////////////////////////////////////
+//                                         //
+//  Press the keyboard "Tap" and "Return"  //
+//                                         //
+/////////////////////////////////////////////
 
+function gameControl() {
+  window.addEventListener("keydown", function(e){
 
-
-
-
-
-
-
-
-window.addEventListener("keydown", function(e){
-/*
-keyCode: 8
-keyIdentifier: "U+0008"
-*/
-    if(e.keyCode === 13 && document.activeElement !== 'text') {
-        e.preventDefault();
-        alert('Prevent page from going back');
-        randomTwo += 1;
+    // Player One, Press Tab to play.
+    if(e.keyCode === 9 && document.activeElement !== 'text') {
+      e.preventDefault();
+      console.log("Player One" + randomOne);
+      randomOne += 10;
+      for (var i = 0; i < playerOneWidth.length; i++) {
+        playerOneWidth[i].style.width = randomOne + '%';
+      }
     }
-});
+
+    // Player Two, Press Enter to play.
+    if(e.keyCode === 13 && document.activeElement !== 'text') {
+      e.preventDefault();
+      console.log("Player Two" + randomTwo);
+      randomTwo += 10;
+      for (var i = 0; i < playerTwoWidth.length; i++) {
+        playerTwoWidth[i].style.width = randomTwo + '%';
+      }
+    }
+
+    // Display the game score for both players.
+    var playerOneScore = document.querySelector('#OneScore'); playerOneScore.innerHTML = " " + randomOne;
+    var playerTwoScore = document.querySelector('#TwoScore'); playerTwoScore.innerHTML = " " + randomTwo;
+
+  });
+}
