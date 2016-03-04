1ea9541878372ca095d99d160229cf33c5f5c5dd
JulioMontas <julio86@gmail.com>
2016-03-04 12:21:14 -0500

    new update

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ console.log("O~~~~~~~~    O~~~~     O~~         O~~  O~~~~~      O~~  O~~      O
 
 /*
 
-Round Zero
+Round -Zero
 01.[] Render a game in the browser
 02.[] Switch turns between two players
 03.[] Design logic for winning & visually display which player won
@@ -24,15 +24,49 @@ Round Zero
 12.[]
 
 
+Round +Zero
+01.[/] Make the Global variables
+02.[x] Build the start game function to press the start html and start the game running.
+03.[] start(); then turn the html tag "HEADER" to display = "flex"
+04.[] start(); Turn off the html tag "#gameTable" to none, is only for display purpose
+05.[]
+06.[]
+07.[]
+08.[]
+09.[]
 
 */
 
+// Calling all Global variables
+var started = false;
+var scoreOne = [];
+var scoreTwo = [];
+var gameOptions = [];
 
-// var leftPlayer = document.querySelector("#playerOne");
-// for (var i = 0; i < 50; i++) {
-//   leftPlayer.style.width = "50%" + i;
-//
-// }
-//
-// var leftPlayer = document.querySelector("#playerTwo");
-// leftPlayer.style.width = "50%";
+var header = document.querySelector('header');
+var menu = document.querySelector('nav');
+var displayGameTable = document.querySelector('#gameTable');
+
+var playerOneWidth = document.querySelectorAll('.playerOne');
+var playerTwoWidth = document.querySelectorAll('.playerTwo');
+
+
+// Lets lets play
+var playButton = document.querySelector('#play');
+playButton.addEventListener('click', function() {
+  start();
+});
+
+// Load up everything that you need for the games. ( Your Treasure Box )
+function start() {
+  console.log("Let's Play <--- WIDTH --->");
+  started = true;
+  gameStartDisplay();
+}
+
+function gameStartDisplay() {
+  console.log("Is Header in the MFH!!!");
+  header.style.display = 'flex';
+  menu.style.display = 'none';
+  displayGameTable.style.display = 'none';
+}
